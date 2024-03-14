7baca67a39418bf98afb2c1c6a3b1c6073f572a8
tajtanvvir1 <tanvirah1223@gmail.com>
2024-03-14 22:50:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import BookStore from './Bookstore'
+
+function App() {
+  const books = [
+    {id: 1, name: 'Physics', price: 100},
+    {id: 1, name: 'Chemistry', price: 100},
+    {id: 1, name: 'English', price: 100},
+    {id: 1, name: 'Math', price: 100},
+  ]
+
+  return (
+    <>      
+      <h1>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
